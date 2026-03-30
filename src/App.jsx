df546a3d25326424e09caa8724b78bc8991581d3
Syed Ahad Imran <syedahad1099@gmail.com>
2026-03-31 02:01:54 +0500

    setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "40px" }}>
+      <h1>My First React Site</h1>
+
+      <h2>About</h2>
+      <p>I am learning React step by step.</p>
+
+      <h2>Services</h2>
+      <p>I will add things here later.</p>
+
+      <h2>Contact</h2>
+      <p>Email: test@example.com</p>
+    </div>
+  );
+}
+
+export default App;
